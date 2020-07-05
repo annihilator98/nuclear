8079a201b4fdac3638e56dc0030b8ca1f1d61999
Anesthesia10 <domnichnastya09@gmail.com>
2020-07-05 19:13:37 +0300

    arrays new home work

--- js/annjs.js
+++ js/annjs.js
@@ -1,6 +1,6 @@
 
 'use strict';
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 let personalMovieDB = {
  count: numberOfFilms,
@@ -10,12 +10,27 @@ let personalMovieDB = {
  privat: false //булинговое значение
 };
 
-const a = prompt('Один из последних фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+    if (a != null && b != null  && a !='' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;//возвращает на одну итерацию цикла назад
+    }
+}
+
+ if (personalMovieDB.count < 10){
+     console.log("Просмотрено довольно мало фильмов");  
+ }  else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+ }  else if (personalMovieDB.count >= 30){
+     console.log("Вы киноман");
+ }  else {
+     console.log("Произошла ошибка");
+ }
 
 console.log(personalMovieDB);
